f26d09356a3629cd5617f1fefc8755960b99a2c8
osxi <osxi@users.noreply.github.com>
2015-08-20 08:40:26 -0500

    update demo for forward barf + slurp

--- index.js
+++ index.js
@@ -8,19 +8,26 @@
   }
 }(this, function() {
 (function(root, factory) {
+  'use strict';
+
   if (typeof define === 'function' && define.amd) {
-    define([], factory);
+    define(['jquery'], factory);
   } else if (typeof exports === 'object') {
-    module.exports = factory();
+    module.exports = factory(require('jquery'));
   } else {
-    root.returnExports = factory();
+    root.Index = factory(root.jQuery);
   }
-}(this, function() {
+}(this, function($) {
   'use strict';
 
   $.fn.barfBackward = function() {
     var firstChild = $(this).children().first();
     return $(this).before(firstChild);
+  };
+
+  $.fn.barfForward = function() {
+    var lastChild = $(this).children().last();
+    return $(this).after(lastChild);
   };
 
   $.fn.slurpBackward = function() {
@@ -29,9 +36,17 @@
       .prependTo($(this));
   };
 
+  $.fn.slurpForward = function() {
+    return $(this)
+      .next()
+      .appendTo($(this));
+  };
+
   return {
     barfBackward: $.fn.barfBackward,
-    slurpBackward: $.fn.slurpBackward
+    barfForward: $.fn.barfForward,
+    slurpBackward: $.fn.slurpBackward,
+    slurpForward: $.fn.slurpForward
   };
 }));
 
