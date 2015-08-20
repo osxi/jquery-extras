c55ce3ec338849bbbc6f13063741669573ccec50
osxi <osxi@users.noreply.github.com>
2015-08-20 07:26:45 -0500

    fix export error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   } else if (typeof exports === 'object') {
     module.exports = factory();
   } else {
-    root.returnExports = factory();
+    root.Index = factory();
   }
 }(this, function() {
   'use strict';
